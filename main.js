c73d90da2ac3502a18a1db0820c37d05f9d88d08
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-25 22:42:24 +0300

    added text-dexoration: line throuhgt

--- main.js
+++ main.js
@@ -1,17 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
   let pen = document.querySelector('.pencil'),
     text = document.querySelector('.add__text'),
-    list = document.querySelector('.list');
+    list = document.querySelector('.list'),
+    i = 0;
 
   pen.addEventListener('click', () => {
     list.textContent = '';
   });
 
   text.addEventListener('keydown', (e) => {
+    /* console.log(e.value); */
+
     if (e.code === 'Enter') {
       list.innerHTML += `<li>- ${e.target.value}</li>`;
       text.value = '';
     }
   });
+
+  list.addEventListener('click', (event) => {
+    let e = event.target;
+
+    if (i % 2 == 0) {
+      i++;
+      e.style.cssText = 'text-decoration: line-through;';
+    } else {
+      i++;
+      e.style.cssText = 'text-decoration: none;';
+    }
+  });
 });
-/* Попробовать также с textContent */
