5f755e8f40363df3c3a6e370b2e9ab34bb6f6aea
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-04-27 19:26:35 +0300

    finished projext fully

--- main.js
+++ main.js
@@ -2,20 +2,28 @@ window.addEventListener('DOMContentLoaded', () => {
   let pen = document.querySelector('.pencil'),
     text = document.querySelector('.add__text'),
     list = document.querySelector('.list'),
+    add = document.querySelector('.add__mobile'),
     i = 0;
 
   pen.addEventListener('click', () => {
     list.textContent = '';
   });
 
-  text.addEventListener('keydown', (e) => {
-
-    if (e.code === 'Enter')
-      if (e.target.value) {
-        list.innerHTML += `<li>- ${e.target.value}</li>`;
+  text.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+      const e = event.target;
+      if (e.value.trim()) {
+        list.innerHTML += `<li class="show">- ${e.value}</li>`;
         text.value = '';
       }
-    
+    }
+  });
+
+  add.addEventListener('click', (event) => {
+    if (text.value.trim()) {
+      list.innerHTML += `<li class="show">- ${text.value}</li>`;
+      text.value = '';
+    }
   });
 
   list.addEventListener('click', (event) => {
