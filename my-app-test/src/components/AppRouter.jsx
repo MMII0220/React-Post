dcfa267ecebeb792bd2d8197300251f8636d1247
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-10-21 07:56:57 +0300

    fix: pages posts is main page

--- my-app-test/src/components/AppRouter.jsx
+++ my-app-test/src/components/AppRouter.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import {
-    Route,
-    Routes,
-    Navigate,
-} from "react-router-dom";
+import { Route, Routes, Navigate } from 'react-router-dom';
 import About from '../pages/About';
 import Posts from './../pages/Posts';
 import Main from '../pages/Main';
 
-
 const AppRouter = () => {
-    return (
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/posts" element={<Posts />} />
+  return (
+    <Routes>
+      <Route path='/about' element={<About />} />
+      <Route path='/main' element={<Main />} />
 
-          <Route path="*" element={<Navigate to="/main" />} />
-          <Route path="/main" element={<Main />} />
-        </Routes>
-    );
+      <Route path='*' element={<Navigate to='/posts' />} />
+      <Route path='/posts' element={<Posts />} />
+    </Routes>
+  );
 };
 
 export default AppRouter;
--- my-app-test/src/components/UI/Navbar/Navbar.jsx
+++ my-app-test/src/components/UI/Navbar/Navbar.jsx
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <nav className="navbar__links">
-                <Link className='link' to="/about">About</Link>
-                <Link className='link' to="/posts">Posts</Link>
-            </nav>
-      </div>
-    );
+  return (
+    <div className='navbar'>
+      <nav className='navbar__links'>
+        <Link className='link' to='/posts'>
+          Posts
+        </Link>
+        <Link className='link' to='/about'>
+          About
+        </Link>
+        <Link className='link' to='/main'>
+          Main
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
